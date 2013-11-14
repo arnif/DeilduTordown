f29579f48a3903365019af4b92b7dd6a9f43465c
arnif <arnifa@gmail.com>
2013-11-14 15:41:42 +0000

    added

---
+++ README.md
@@ -0,0 +1,8 @@
+HOW TO:
+
+Farðu í
+chrome://settings/cookies
+
+Leita eftir deildu
+skrifar niður value á uid og pass
+setur það í uid og pass value í tordown.py (efst)
---
+++ tordown.py
@@ -0,0 +1,73 @@
+import urllib2
+import os
+import feedparser
+import re
+import sys
+
+UID = 'xxxx'
+PASS = 'yyyyyyyyyyyy'
+
+if (len(sys.argv) != 3):
+	print "To few arguments, should be tordown.py path/to/save/file 'link_to_rssfeed'"
+	sys.exit(1)
+
+rss_feed = sys.argv[2]
+
+save_file = sys.argv[1]
+
+d = feedparser.parse(rss_feed)
+
+regfile = open('regex', 'r').read().splitlines()
+	
+
+def downloadshow(wantedShow):
+
+	for torr in d.entries:
+		rssTitle = torr.title
+		matchObj = re.search( wantedShow, rssTitle, re.IGNORECASE) 
+
+		if matchObj:
+			#check log file, if not found add to log file and download file
+			if (checklog(rssTitle)):
+				#dont download
+
+				continue
+			else:
+				#download
+
+				theLink = torr.link
+				fileName = torr.link.replace(' ','%20').split('?')[0]
+				fileName = fileName.split('/')
+
+				try:
+					opener = urllib2.build_opener()
+					opener.addheaders.append(('Cookie', 'uid=' + UID + ';pass=' + PASS + ''))
+					f = opener.open(theLink.replace(' ','%20').encode('utf-8'))
+
+					try:
+						with open(save_file + '/' + fileName[-1], "wb") as local_file:
+							local_file.write(f.read())
+					except:
+						print "Failed to save file"
+				except Exception:
+					print "Failed to download"
+
+
+
+def checklog(rssTitle):
+
+	if rssTitle.encode('utf-8') in open('torr.log').read():
+    		return True
+    	else:
+    		open('torr.log', 'a').write(rssTitle.encode('utf-8') + '\n')
+    		return False
+			
+
+for wantedShow in regfile:
+	downloadshow(wantedShow)	
+	
+
+
+
+
+
